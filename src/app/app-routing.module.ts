accdfda18bad644f928870e7bdcba63d2f2045b3
Hieu Do <mr.hieudt@gmail.com>
2017-07-02 08:40:12 -0500

    Create home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: HomeComponent
   }
 ];
 
