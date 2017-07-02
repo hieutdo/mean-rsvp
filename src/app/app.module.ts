ca20ab40c02c4fb20ca3ce8993d30e907cd632f0
Hieu Do <mr.hieudt@gmail.com>
2017-07-02 11:24:19 -0500

    Create layout including off-canvas navigation panel, hamburger menu toggle, header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  @Output() navToggled = new EventEmitter();
+  navOpen = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events
+      .filter(event => event instanceof NavigationStart && this.navOpen)
+      .subscribe(event => this.toggleNav());
+  }
+
+  toggleNav() {
+    this.navOpen = !this.navOpen;
+    this.navToggled.emit(this.navOpen);
+  }
+
+}
